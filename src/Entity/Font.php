3af901214a1a20ffd13f1772a52651089aa35897
Ashok Modi <btmash@gmail.com>
2016-03-29 09:38:38 -0700

    Start adding/removing properties. Figure out the interface portion some more.

--- src/Entity/Font.php
+++ src/Entity/Font.php
@@ -42,12 +42,11 @@ use Drupal\user\UserInterface;
  *   base_table = "fontyourface_font",
  *   admin_permission = "administer font entities",
  *   entity_keys = {
- *     "id" = "id",
+ *     "id" = "fid",
  *     "label" = "name",
  *     "uuid" = "uuid",
- *     "uid" = "user_id",
- *     "langcode" = "langcode",
- *     "status" = "status",
+ *     "pid" = "pid",
+ *     "url" = "url",
  *   },
  *   links = {
  *     "canonical" = "/admin/appearance/font/{font}",
@@ -66,9 +65,6 @@ class Font extends ContentEntityBase implements FontInterface {
    */
   public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
     parent::preCreate($storage_controller, $values);
-    $values += array(
-      'user_id' => \Drupal::currentUser()->id(),
-    );
   }
 
   /**
@@ -104,53 +100,8 @@ class Font extends ContentEntityBase implements FontInterface {
   /**
    * {@inheritdoc}
    */
-  public function getOwner() {
-    return $this->get('user_id')->entity;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getOwnerId() {
-    return $this->get('user_id')->target_id;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setOwnerId($uid) {
-    $this->set('user_id', $uid);
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setOwner(UserInterface $account) {
-    $this->set('user_id', $account->id());
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function isPublished() {
-    return (bool) $this->getEntityKey('status');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setPublished($published) {
-    $this->set('status', $published ? NODE_PUBLISHED : NODE_NOT_PUBLISHED);
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
-    $fields['id'] = BaseFieldDefinition::create('integer')
+    $fields['fid'] = BaseFieldDefinition::create('integer')
       ->setLabel(t('ID'))
       ->setDescription(t('The ID of the Font entity.'))
       ->setReadOnly(TRUE);
@@ -158,36 +109,9 @@ class Font extends ContentEntityBase implements FontInterface {
       ->setLabel(t('UUID'))
       ->setDescription(t('The UUID of the Font entity.'))
       ->setReadOnly(TRUE);
-
-    $fields['user_id'] = BaseFieldDefinition::create('entity_reference')
-      ->setLabel(t('Authored by'))
-      ->setDescription(t('The user ID of author of the Font entity.'))
-      ->setRevisionable(TRUE)
-      ->setSetting('target_type', 'user')
-      ->setSetting('handler', 'default')
-      ->setDefaultValueCallback('Drupal\node\Entity\Node::getCurrentUserId')
-      ->setTranslatable(TRUE)
-      ->setDisplayOptions('view', array(
-        'label' => 'hidden',
-        'type' => 'author',
-        'weight' => 0,
-      ))
-      ->setDisplayOptions('form', array(
-        'type' => 'entity_reference_autocomplete',
-        'weight' => 5,
-        'settings' => array(
-          'match_operator' => 'CONTAINS',
-          'size' => '60',
-          'autocomplete_type' => 'tags',
-          'placeholder' => '',
-        ),
-      ))
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayConfigurable('view', TRUE);
-
-    $fields['name'] = BaseFieldDefinition::create('string')
-      ->setLabel(t('Name'))
-      ->setDescription(t('The name of the Font entity.'))
+    $fields['pid'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Provider ID'))
+      ->setDescription(t('The font provider ID.'))
       ->setSettings(array(
         'max_length' => 50,
         'text_processing' => 0,
@@ -205,19 +129,65 @@ class Font extends ContentEntityBase implements FontInterface {
       ->setDisplayConfigurable('form', TRUE)
       ->setDisplayConfigurable('view', TRUE);
 
-    $fields['status'] = BaseFieldDefinition::create('boolean')
-      ->setLabel(t('Publishing status'))
-      ->setDescription(t('A boolean indicating whether the Font is published.'))
-      ->setDefaultValue(TRUE);
-
-    $fields['langcode'] = BaseFieldDefinition::create('language')
-      ->setLabel(t('Language code'))
-      ->setDescription(t('The language code for the Font entity.'))
-      ->setDisplayOptions('form', array(
-        'type' => 'language_select',
-        'weight' => 10,
+    $fields['url'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Font URL'))
+      ->setDescription(t('A URL for the font.'))
+      ->setSettings(array(
+        'max_length' => 255,
+        'text_processing' => 0,
       ))
-      ->setDisplayConfigurable('form', TRUE);
+      ->setDefaultValue('')
+      ->setDisplayOptions('view', array(
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -4,
+      ))
+      ->setDisplayOptions('form', array(
+        'type' => 'string_textfield',
+        'weight' => -4,
+      ))
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Name'))
+      ->setDescription(t('The name of the Font entity.'))
+      ->setSettings(array(
+        'max_length' => 255,
+        'text_processing' => 0,
+      ))
+      ->setDefaultValue('')
+      ->setDisplayOptions('view', array(
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -4,
+      ))
+      ->setDisplayOptions('form', array(
+        'type' => 'string_textfield',
+        'weight' => -4,
+      ))
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['css_family'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('CSS Family'))
+      ->setDescription(t('CSS family for the font.'))
+      ->setSettings(array(
+        'max_length' => 255,
+        'text_processing' => 0,
+      ))
+      ->setDefaultValue('')
+      ->setDisplayOptions('view', array(
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -4,
+      ))
+      ->setDisplayOptions('form', array(
+        'type' => 'string_textfield',
+        'weight' => -4,
+      ))
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
 
     $fields['created'] = BaseFieldDefinition::create('created')
       ->setLabel(t('Created'))
--- src/FontInterface.php
+++ src/FontInterface.php
@@ -16,7 +16,7 @@ use Drupal\user\EntityOwnerInterface;
  *
  * @ingroup fontyourface
  */
-interface FontInterface extends ContentEntityInterface, EntityChangedInterface, EntityOwnerInterface {
+interface FontInterface extends ContentEntityInterface, EntityChangedInterface {
   // Add get/set methods for your configuration properties here.
   /**
    * Gets the Font name.
@@ -55,26 +55,5 @@ interface FontInterface extends ContentEntityInterface, EntityChangedInterface, 
    *   The called Font entity.
    */
   public function setCreatedTime($timestamp);
-
-  /**
-   * Returns the Font published status indicator.
-   *
-   * Unpublished Font are only visible to restricted users.
-   *
-   * @return bool
-   *   TRUE if the Font is published.
-   */
-  public function isPublished();
-
-  /**
-   * Sets the published status of a Font.
-   *
-   * @param bool $published
-   *   TRUE to set this Font to published, FALSE to set it to unpublished.
-   *
-   * @return \Drupal\fontyourface\FontInterface
-   *   The called Font entity.
-   */
-  public function setPublished($published);
 
 }

859ff38726cf5aa7f5f01795255fbc7f6d35b412
Ashok Modi <btmash@gmail.com>
2016-03-29 09:55:33 -0700

    Show fyf in appearance section.

--- src/FontHtmlRouteProvider.php
+++ src/FontHtmlRouteProvider.php
@@ -114,7 +114,7 @@ class FontHtmlRouteProvider extends AdminHtmlRouteProvider {
    */
   protected function getSettingsFormRoute(EntityTypeInterface $entity_type) {
     if (!$entity_type->getBundleEntityType()) {
-      $route = new Route("/admin/structure/{$entity_type->id()}/settings");
+      $route = new Route("/admin/appearance/{$entity_type->id()}/settings");
       $route
         ->setDefaults([
           '_form' => 'Drupal\fontyourface\Form\FontSettingsForm',

cb51745bd9e067bfda9ffbd2a637ba72d42909e6
Ashok Modi <btmash@gmail.com>
2016-03-30 17:03:31 -0700

    Add inline css asset.

---
+++ src/Asset/FontYourFaceInlineCssCollectionRenderer.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\fontyourface\Asset\FontYourFaceInlineCssCollectionRenderer.
+ */
+
+namespace Drupal\fontyourface\Asset;
+
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Asset\CssCollectionRenderer;
+
+class FontYourFaceInlineCssCollectionRenderer extends CssCollectionRenderer {
+  
+}

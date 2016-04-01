1ce848bf9655bfef7c9e15f928e424a2382fc2a6
Ashok Modi <btmash@gmail.com>
2016-04-01 11:22:19 -0700

    Remove the base view table.

--- src/Entity/FontViewsData.php
+++ src/Entity/FontViewsData.php
@@ -20,12 +20,6 @@ class FontViewsData extends EntityViewsData implements EntityViewsDataInterface 
   public function getViewsData() {
     $data = parent::getViewsData();
 
-    $data['font']['table']['base'] = array(
-      'field' => 'id',
-      'title' => $this->t('Font'),
-      'help' => $this->t('The Font ID.'),
-    );
-
     return $data;
   }
 

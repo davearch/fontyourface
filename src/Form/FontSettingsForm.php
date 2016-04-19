2d636e8de1791a039b260f47bc1b8f99fee64196
Ashok Modi <btmash@gmail.com>
2016-04-18 23:51:50 -0700

    Load all fonts to page by default. Make user disable.

--- src/Form/FontSettingsForm.php
+++ src/Form/FontSettingsForm.php
@@ -7,7 +7,7 @@
 
 namespace Drupal\fontyourface\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -17,7 +17,14 @@ use Drupal\Core\Form\FormStateInterface;
  *
  * @ingroup fontyourface
  */
-class FontSettingsForm extends FormBase {
+class FontSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['fontyourface.settings'];
+  }
 
   /**
    * Returns a unique string identifying the form.
@@ -27,23 +34,6 @@ class FontSettingsForm extends FormBase {
    */
   public function getFormId() {
     return 'Font_settings';
-  }
-
-  /**
-   * Form submission handler.
-   *
-   * @param array $form
-   *   An associative array containing the structure of the form.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $values = $form_state->getValues();
-    $op = $values['op'];
-    if ($op == 'Import all fonts') {
-      $module_handler = \Drupal::moduleHandler();
-      $module_handler->invokeAll('fontyourface_import');
-    }
   }
 
   /**
@@ -58,18 +48,43 @@ class FontSettingsForm extends FormBase {
    *   Form definition array.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('fontyourface.settings');
     $form['Font_settings']['#markup'] = 'Settings form for @font-your-face. Support modules can use this form for settings or to import fonts.';
-    $form['save'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Save'),
-      '#weight' => 10,
+    $form['load_all_fonts'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Load all enabled fonts'),
+      '#default_value' => (int) $config->get('load_all_fonts'),
+      '#description' => $this->t('This will load all fonts that have been enabled regardless of theme. Warning: this may add considerable download weight to your pages depending on the number of enabled fonts'),
     ];
     $form['import'] = [
       '#type' => 'submit',
       '#value' => $this->t('Import all fonts'),
       '#weight' => 10,
     ];
-    return $form;
+    return parent::buildForm($form, $form_state);;
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $op = (string) $values['op'];
+    if ($op == $this->t('Import all fonts')) {
+      $module_handler = \Drupal::moduleHandler();
+      $module_handler->invokeAll('fontyourface_import');
+    }
+    if ($op == $this->t('Save configuration')) {
+      $config = $this->config('fontyourface.settings')
+        ->set('load_all_fonts', $values['load_all_fonts'])
+        ->save();
+      parent::submitForm($form, $form_state);
+    }
   }
 
 }

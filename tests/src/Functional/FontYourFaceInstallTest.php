b8910e9b8d650fc24c11f1917fc4144c5715d341
BTMash <btmash@gmail.com>
2016-09-03 06:28:00 +0000

    Provide asserts that font sections work

--- tests/src/Functional/FontYourFaceInstallTest.php
+++ tests/src/Functional/FontYourFaceInstallTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\fontyourface\Functional;
 
+use Drupal\Core\Url;
 use Drupal\Tests\BrowserTestBase;
 
 /**
@@ -19,12 +20,40 @@ class FontYourFaceInstallTest extends BrowserTestBase {
   public static $modules = array('fontyourface');
 
   /**
-   * Tests @font-your-face install.
+   * A test user with permission to access the @font-your-face sections.
+   *
+   * @var \Drupal\user\UserInterface
    */
-  public function testFontYourFaceInstall() {
-    $admin_user = $this->drupalCreateUser(array('administer font entities'));
-    $this->drupalLogin($admin_user);
+  protected $adminUser;
 
-    $this->drupalGet('admin/appearance/font');
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Create and log in an administrative user.
+    $this->adminUser = $this->drupalCreateUser([
+      'administer font entities',
+    ]);
+    $this->drupalLogin($this->adminUser);
+  }
+
+  /**
+   * Tests @font-your-face install and admin page shows up.
+   */
+  public function testFontYourFaceSections() {
+    // Main font selection page.
+    $this->drupalGet(Url::fromRoute('entity.font.collection'));
+    $this->assertSession()->pageTextContains(t('Font Selector'));
+    // Font settings page.
+    $this->drupalGet(Url::fromRoute('font.settings'));
+    $this->assertSession()->pageTextContains(t('Settings form for @font-your-face. Support modules can use this form for settings or to import fonts.'));
+    // Font display page.
+    $this->drupalGet(Url::fromRoute('entity.font_display.collection'));
+    $this->assertSession()->pageTextContains(t('There is no Font display yet.'));
+    // Font display add page.
+    $this->drupalGet(Url::fromRoute('entity.font_display.add_form'));
+    $this->assertSession()->pageTextContains(t('Please select atleast one font before picking a font style.'));
   }
 }

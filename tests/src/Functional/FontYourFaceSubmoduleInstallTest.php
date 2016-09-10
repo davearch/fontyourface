844b403996731a9b571df3b04e7a74eb510ae5a9
Ashok Modi <btmash@gmail.com>
2016-09-09 17:14:34 -0700

    Add test to verify enabling fonts works.

--- tests/src/Functional/FontYourFaceSubmoduleInstallTest.php
+++ tests/src/Functional/FontYourFaceSubmoduleInstallTest.php
@@ -65,21 +65,53 @@ class FontYourFaceSubmoduleInstallTest extends WebTestBase {
    * Tests importing fonts from websafe_fonts_test.
    */
   public function testImportWebSafeFonts() {
-    // Assert no fonts exist to start
+    // Assert no fonts exist to start.
     $this->drupalGet(Url::fromRoute('entity.font.collection'));
     $this->assertNoText('Arial');
 
     $this->drupalPostForm(Url::fromRoute('font.settings'), [], t('Import from websafe_fonts_test'));
     $this->assertResponse(200);
     $this->assertUrl(Url::fromRoute('font.settings'));
-    $this->assertRaw(t('Finished importing fonts.'));
+    $this->assertText(t('Finished importing fonts.'));
 
     // Assert all fonts were imported.
     $this->drupalGet(Url::fromRoute('entity.font.collection'));
-    $this->assertRaw('Arial');
-    $this->assertRaw('Verdana');
-    $this->assertRaw('Courier New');
-    $this->assertRaw('Georgia');
+    $this->assertText('Arial');
+    $this->assertText('Verdana');
+    $this->assertText('Courier New');
+    $this->assertText('Georgia');
+
+    // Assert fonts load on font collection page.
+    $this->assertRaw('<meta name="Websafe Font" content="Arial" />');
+    $this->assertRaw('<meta name="Websafe Font" content="Courier New" />');
+    $this->assertRaw('<meta name="Websafe Font" content="Georgia" />');
+    $this->assertRaw('<meta name="Websafe Font" content="Verdana" />');
+
+    // ENsure font is not loaded on front page because font is not enabled.
+    $this->drupalGet('<front>');
+    $this->assertNoRaw('<meta name="Websafe Font" content="Arial" />');
+  }
+
+  /**
+   * Tests enabling and seeing fonts load.
+   */
+  /**
+   * Tests importing fonts from websafe_fonts_test.
+   */
+  public function testEnableWebSafeFonts() {
+    // Assert no fonts load to start.
+    $this->drupalGet('/node');
+    $this->assertNoRaw('<meta name="Websafe Font" content="Arial" />');
+
+    $this->drupalPostForm(Url::fromRoute('font.settings'), ['load_all_enabled_fonts' => 1], t('Import from websafe_fonts_test'));
+    $this->drupalGet(url::fromRoute('entity.font.enable', ['font' => 1, 'js' => 'nojs']));
+    $this->assertText('Font Arial successfully enabled');
+
+    // Flush the caches. Not an issue in prod but seems to be in simpletest. Will keep an eye on it.
+    $this->resetAll();
+
+    $this->drupalGet('/node');
+    $this->assertRaw('<meta name="Websafe Font" content="Arial" />');
   }
 
 }

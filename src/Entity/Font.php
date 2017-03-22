ef72333ad1e549b591ad8f3a38a08c94d87055e1
Ashok Modi <btmash@gmail.com>
2017-03-22 10:06:53 -0700

    PHPCS: Remove unused use statement.

--- src/Entity/Font.php
+++ src/Entity/Font.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\fontyourface\Entity;
 
-use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\EntityChangedTrait;

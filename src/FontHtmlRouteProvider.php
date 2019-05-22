5970484357dafaab5e84483d5f95044410f0f0ec
florenttorregrosa <florenttorregrosa@2388214.no-reply.drupal.org>
2019-05-21 20:37:58 -0700

    Issue #2941213 by Grimreaper: Declare font.settings route using yml file instead of dynamically

--- src/FontHtmlRouteProvider.php
+++ src/FontHtmlRouteProvider.php
@@ -30,10 +30,6 @@ class FontHtmlRouteProvider extends AdminHtmlRouteProvider {
       $collection->add("entity.{$entity_type_id}.add_form", $add_form_route);
     }
 
-    if ($settings_form_route = $this->getSettingsFormRoute($entity_type)) {
-      $collection->add("$entity_type_id.settings", $settings_form_route);
-    }
-
     return $collection;
   }
 
@@ -98,29 +94,4 @@ class FontHtmlRouteProvider extends AdminHtmlRouteProvider {
       return $route;
     }
   }
-
-  /**
-   * Gets the settings form route.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
-   *   The entity type.
-   *
-   * @return \Symfony\Component\Routing\Route|null
-   *   The generated route, if available.
-   */
-  protected function getSettingsFormRoute(EntityTypeInterface $entity_type) {
-    if (!$entity_type->getBundleEntityType()) {
-      $route = new Route("/admin/appearance/{$entity_type->id()}/settings");
-      $route
-        ->setDefaults([
-          '_form' => 'Drupal\fontyourface\Form\FontSettingsForm',
-          '_title' => "{$entity_type->getLabel()} settings",
-        ])
-        ->setRequirement('_permission', $entity_type->getAdminPermission())
-        ->setOption('_admin_route', TRUE);
-
-      return $route;
-    }
-  }
-
 }
